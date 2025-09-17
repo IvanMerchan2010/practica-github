08bc0e3d55b861b0407f4929a014a36be19addbe
IvanMerchan2010 <ivan.merchan@campus.xaloc.org>
2025-09-17 09:42:28 +0200

    Add files via upload
    El archivo se ha subido correctamente

---
+++ Proyecto 2.py
@@ -0,0 +1,20 @@
+nombre=input()
+apellido1=input("introduce tu primer apellido")
+apellido2=input("introduce tu segundo apellido")
+nombre_completo= nombre + " " + apellido1 + " " + apellido2
+print(nombre_completo)
+
+suma=var2+var3
+print(f"La suma de {var2} + {var3} es igual a ", suma)
+resta=var2-var3
+print=(f"La resta de {var2} - {var3} es igual a", resta)
+multiplicación=var2*var3
+print=(f"La multiplicación de {var2} * {var3} es igual a", multiplicación)
+division=var2/var3
+print=(f"La division de {var2} / {var3} es igual a", division)
+division_entera=var2//var3
+print=(f"La division_entera de {var2} / {var3} es igual a", division_entera)
+modulo_division=var2%var3
+print=(f"El modulo_division de {var2} / {var3} es igual a", modulo_division)
+potencia=var2**var3
+print=(f"La potencia de {var2}  {var3} es igual a", potencia)
